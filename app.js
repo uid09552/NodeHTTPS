b5edf06163cefda2bfacc051cfcee53b611764bb
uid09552 <max.rgbg@gmail.com>
2016-04-19 12:57:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 /**
  * Created by Max on 06.10.2015.
  */
-var https = require('https');
-var fs = require('fs');
+var https = require('http');
 
-var hskey = fs.readFileSync('key.pem');
-var hscert = fs.readFileSync('cert.pem')
 
-var options = {
-    key: hskey,
-    cert: hscert
-};
 
-https.createServer(options, function (req, res) {
+http.createServer(options, function (req, res) {
     res.writeHead(200);
-    res.end("Hi from HTTPS");
+    res.end("Hi from HTTP");
 }).listen(process.env.VCAP_APP_PORT || 3000);
 
 
